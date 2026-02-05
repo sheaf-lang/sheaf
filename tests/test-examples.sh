84efabf0a2887264540303c22f6a83a2839c5b06
dbrll <dbrll@users.noreply.github.com>
2026-02-05 23:01:52 +0100

    tests: update test-examples.sh to autonomous sheaf examples

--- tests/test-examples.sh
+++ tests/test-examples.sh
@@ -1,19 +1,11 @@
 cd ../examples
 
-for TEST in baregpt clevr hydra; do
+for TEST in baregpt clevr hydra mlp; do
 	cd $TEST
 	echo "Running $TEST..."
-	if python ./run.py > /dev/null ; then
+	if sheaf ./run.shf > /dev/null ; then
 	echo "$TEST OK"
 	else	echo "$TEST FAIL"
 	fi
-
 	cd ..
 done
-
-# mlp
-if sheaf ./mlp/run.shf  > /dev/null ; then
-	echo "mlp OK"
-else
-   	echo "mlp FAIL"
-fi
